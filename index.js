dfbeec791ae6c7d7800d25254d5a1a964ebc2768
liunian <dengjij@gmail.com>
2016-03-30 09:02:43 +0800

    format and ignore files

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 var ejs = require('ejs'),
-    through = require('through');
+  through = require('through');
 
 var filenamePattern = /\.(html|ejs)$/;
 
 var wrap = function(template) {
-    return 'module.exports=(function() {var t = ' + template + '; return function(l) { return t(l) }}())';
+  return 'module.exports=(function() {var t = ' + template + '; return function(l) { return t(l) }}())';
 };
 
 module.exports = function(file) {
-    if (!filenamePattern.test(file)) return through();
+  if (!filenamePattern.test(file)) return through();
 
-    var input = '';
-    var write = function(buffer) {
-        input += buffer;
-    };
+  var input = '';
+  var write = function(buffer) {
+    input += buffer;
+  };
 
-    var end = function() {
-        this.queue(wrap(ejs.compile(input, {
-            client: true, 
-            compileDebug: false, 
-            filename: file,
-            rmWhitespace: true
-        })));
-        this.queue(null);
-    };
+  var end = function() {
+    this.queue(wrap(ejs.compile(input, {
+      client: true,
+      compileDebug: false,
+      filename: file,
+      rmWhitespace: true
+    })));
+    this.queue(null);
+  };
 
-    return through(write, end);
-}
+  return through(write, end);
+};
